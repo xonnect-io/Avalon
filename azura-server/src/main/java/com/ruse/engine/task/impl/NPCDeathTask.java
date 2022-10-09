f669b23efc5d64ebb4905ba05de75105b7bd34fa
Avalon <77313597+MyLifeWrittenInCode@users.noreply.github.com>
2022-10-09 12:37:34 -0400

    latest

--- azura-server/src/main/java/com/ruse/engine/task/impl/NPCDeathTask.java
+++ azura-server/src/main/java/com/ruse/engine/task/impl/NPCDeathTask.java
@@ -239,7 +239,6 @@ public class NPCDeathTask extends Task {
                         if (npc.getId() == PennywiseSpawn.NPC_ID) {
                             PennywiseSpawn.bossAlive = false;
                             PennywiseSpawn.handleDrop(npc);
-                            GlobalBossDrops.handleDrop(npc);
                         }
 
                         if (npc.getId() == 9813) {
