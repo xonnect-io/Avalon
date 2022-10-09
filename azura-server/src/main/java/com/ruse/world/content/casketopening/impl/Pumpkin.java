e1ed83f99d2be4474c43b1bd840be6cd8a46dfda
Avalon <77313597+MyLifeWrittenInCode@users.noreply.github.com>
2022-10-09 01:15:33 -0400

    omg

--- azura-server/src/main/java/com/ruse/world/content/casketopening/impl/Pumpkin.java
+++ azura-server/src/main/java/com/ruse/world/content/casketopening/impl/Pumpkin.java
@@ -7,8 +7,8 @@ public class Pumpkin {
     public static Box[] loot = { //
             new Box(15289, 1, 100),
             new Box(5022, 15_000, 100),
-            new Box(19001, 250, 100),
-            new Box(19864, 1, 100),
+            new Box(19001, 1, 100),
+            new Box(19864, 250, 100),
             new Box(15288, 1, 10),
             new Box(23428, 1, 0.5, true),
             new Box(23430, 1,1, 0.2, true),
