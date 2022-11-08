5c1a952b2752ff7d5e64129ecab4baa13d4fa8eb
Avalon <77313597+MyLifeWrittenInCode@users.noreply.github.com>
2022-11-08 07:34:13 -0500

    bucket fix

--- azura-server/src/main/java/com/ruse/world/entity/impl/GlobalItemSpawner.java
+++ azura-server/src/main/java/com/ruse/world/entity/impl/GlobalItemSpawner.java
@@ -27,7 +27,7 @@ public class GlobalItemSpawner {
         nullCheckAndSpawn(player, new Item(946, 1), new Position(2891, 4120, 0));
         nullCheckAndSpawn(player, new Item(1923, 1), new Position(2882, 4120, 0));
         nullCheckAndSpawn(player, new Item(7546, 1), new Position(2888, 4122, 0));
-        nullCheckAndSpawn(player, new Item(1935, 1), new Position(2888, 4123, 0));
+        nullCheckAndSpawn(player, new Item(3727, 1), new Position(2888, 4123, 0));
         nullCheckAndSpawn(player, new Item(4682, 1), new Position(2940, 4127, 0));
         nullCheckAndSpawn(player, new Item(558, 1), new Position(3206, 3208, 0));
         nullCheckAndSpawn(player, new Item(7509, 1), ROCKCAKE_POSITION);
