0dd42ac5f00b590d41cd596c2c8ac569410cad0a
Avalon <77313597+MyLifeWrittenInCode@users.noreply.github.com>
2022-12-11 19:42:04 -0500

    updated login decoder

--- Azura-Client/src/main/java/org/necrotic/Configuration.java
+++ Azura-Client/src/main/java/org/necrotic/Configuration.java
@@ -4,7 +4,7 @@ public class Configuration {
 
 
 	public static boolean localHost = false;
-	public static int clientversion = 27;//change this to match the one in LoginDecoder
+	public static int clientversion = 28;//change this to match the one in LoginDecoder
 	public final static String SERVER_HOST() {
 		return localHost ? "127.0.0.1" : "51.81.49.184";
 	}
